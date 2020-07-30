bc8c8a9ba64c9c6affa1e6da3ac3947c304c51ff
CodeFr33k <noreply>
2020-07-30 10:47:44 +0930

    add initial files

---
+++ parseRecords.test.ts
@@ -0,0 +1,78 @@
+import { observable } from 'mobx';
+import parseRecords from './parseRecords';
+
+it('parse record', () => {
+    const lines: string[] = [
+        'abc',
+        '',
+    ];
+    const records = parseRecords(lines);
+    expect(records).toHaveLength(1);
+})
+
+it('add lines to record', () => {
+    const lines: string[] = [
+        'abc',
+        '',
+    ];
+    const records = parseRecords(lines);
+    expect(records[0].lines).toHaveLength(2);
+});
+
+it('parse img', () => {
+     const lines: string[] = [
+        'abc',
+        '(`',
+        'img = http://localhost/image.png',
+        ')',
+    ];
+    const records = parseRecords(lines);
+    expect(records[0].images).toHaveLength(1);
+});
+
+it('parse key value', () => {
+     const lines: string[] = [
+        'abc',
+        '(`',
+        'abc = 123',
+        ')',
+    ];
+    const records = parseRecords(lines);
+    expect(records[0].annotations).toContainEqual({
+        key: 'abc',
+        value: '123',        
+    });
+});
+
+it('parse tag', () => {
+     const lines: string[] = [
+        'abc',
+        '(`',
+        '123',
+        ')',
+    ];
+    const records = parseRecords(lines);
+    expect(records[0].annotations).toContain('123');
+});
+
+it('parse key value on one line', () => {
+     const lines: string[] = [
+        '(`abc = 123)',
+    ];
+    const records = parseRecords(lines);
+    expect(records[0].annotations).toContainEqual({
+        key: 'abc',
+        value: '123',        
+    });
+});
+
+it('parse 2 records from 2 one liners', () => {
+     const lines: string[] = [
+        'abc (`abc = 123)',
+        '',
+        'def (`abc = 123)',
+    ];
+    const records = parseRecords(lines);
+    expect(records).toHaveLength(2);
+});
+
---
+++ parseRecords.ts
@@ -0,0 +1,62 @@
+import Record from './Record';
+import * as util from './util';
+
+export default function  parseRecords(lines: string[]) {
+    const result = lines.reduce((result: any, line: string) => {
+       if(!result.record && line){
+            result.record = new Record();
+            result.records.push(result.record);
+        }
+        if(result.record) {
+            result.record.lines.push(line);
+        }
+        const match = line.match(/\(`(.+)\)/);
+        let tmp = line;
+        if(match) {
+            result.isReadingData = true;
+            tmp = match[1];
+        }
+        else if(line.startsWith('(`')) {
+            result.isReadingData = true;
+            return result;
+        }
+        else if(line.startsWith(')')) {
+            result.isReadingData = false;
+            return result;
+        }
+        if(result.isReadingData) {
+            const match = util.matchKeyValue(tmp);
+            const tag = util.matchTag(tmp);
+            if(match) {
+                result.record.annotations.push({
+                    key: match.key,
+                    value: match.value,
+                });
+                if(match.key === 'img') {
+                    const uris = util.matchUris(match.value);
+                    if(uris.length > 0) {
+                        // NOTE: only supports one image uri currently
+                        // per key-value
+                        result.record.images.push(uris[0]);
+                    }
+                }
+            } else if(tag) {
+                result.record.annotations.push(tag);
+            }
+        }
+        if(match) {
+            result.isReadingData = false;
+        }
+        if(!line && !result.isReadingData) {
+            result.record = undefined;
+            result.isReadingDate = false;
+        }
+        return result;
+    }, {
+        records: [],
+        record: undefined,
+        isReadingData: false,
+    });
+    return result.records.reverse();
+}
+
